a379efee90519fe9d4cd8d328895b16268c5e5c4
Shaomeng Zhang <randomor@gmail.com>
2023-10-21 10:04:32 -0700

    Refactoring the code for index

--- index.js
+++ index.js
@@ -7,25 +7,28 @@ import { hideBin } from 'yargs/helpers';
 
 const argv = yargs(hideBin(process.argv))
   .command('$0 [directoryPath]', 'default command', (yargs) => {
-    yargs.positional('directoryPath', {
-      describe: 'Path to the directory',
+    yargs.positional('inputFolder', {
+      describe: 'Input folder path',
       default: './docs',
     })
   })
-  .option('outputFile', {
+  .option('outputFolder', {
     alias: 'o',
     type: 'string',
-    describe: 'Output file name',
+    describe: 'Output folder path',
     default: './dist',
   })
   .option('tags', {
     alias: 't',
-    type: 'string',
+    type: 'array',
     describe: 'Tags for the operation',
   })
   .argv;
 
-console.log(chalk.blue('Matching .md files from directory: '), chalk.green(argv.directoryPath));
+console.log(chalk.blue('Matching .md files from directory: '), chalk.green(argv.inputFolder));
 console.log(chalk.blue('Filtering matches files by tags: '), chalk.green(argv.tags));
+console.log(chalk.blue('Output folder: '), chalk.green(argv.outputFolder));
 
-processFolder(argv.directoryPath, argv.output, { tags: argv.tags });
+const tagsKeyValue = argv.tags && argv.tags.map((tag) => tag.split(':'));
+
+processFolder(argv.inputFolder, argv.outputFolder, { tags: tagsKeyValue });
