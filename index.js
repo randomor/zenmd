7f27c70b307954814985e7395d591e2dcaf39b9c
Shaomeng Zhang <randomor@gmail.com>
2023-10-21 17:19:36 -0700

    Adding force option to override output folder

--- index.js
+++ index.js
@@ -25,36 +25,47 @@ const argv = yargs(hideBin(process.argv))
     type: 'array',
     describe: 'Tags for the operation',
   })
+  .option('force', {
+    alias: 'f',
+    type: 'boolean',
+    describe: 'Force reset of output folder',
+  })
   .argv;
 
 console.log(chalk.blue('Matching .md files from directory: '), chalk.green(argv.inputFolder));
 console.log(chalk.blue('Filtering matches files by tags: '), chalk.green(argv.tags));
 console.log(chalk.blue('Output folder: '), chalk.green(argv.outputFolder));
 
-// Check if output folder is empty, if not, ask for confirmation before erasing it
-const outputFolderFiles = await fs.readdir(argv.outputFolder);
-if (outputFolderFiles.length > 0) {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.question(
-    chalk.red(
-      `Output folder ${argv.outputFolder} is not empty, do you want to continue? (y/n) `
-    ),
-    async (answer) => {
-      if (answer !== 'y') {
-        console.log(chalk.red('Exiting...'));
-        process.exit(1);
-      }
-      rl.close();
-
-      // erase output folder
-      await fs.rm(argv.outputFolder, { recursive: true });
-
-      const tagsKeyValue = argv.tags && argv.tags.map((tag) => tag.split(':'));
-
-      processFolder(argv.inputFolder, argv.outputFolder, { tags: tagsKeyValue });
-    }
-  );
+const startProcessing = async () => {
+  // erase output folder
+  await fs.rm(argv.outputFolder, { recursive: true });
+  
+  const tagsKeyValue = argv.tags && argv.tags.map((tag) => tag.split(':'));
+  await processFolder(argv.inputFolder, argv.outputFolder, { tags: tagsKeyValue });
 }
+
+if (argv.force) {
+  await startProcessing();
+} else {
+  // Check if output folder is empty, if not, ask for confirmation before erasing it
+  const outputFolderFiles = await fs.readdir(argv.outputFolder);
+  if (outputFolderFiles.length > 0) {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    rl.question(
+      chalk.red(
+        `Output folder ${argv.outputFolder} is not empty, do you want to continue? (y/n) `
+      ),
+      async (answer) => {
+        if (answer !== 'y') {
+          console.log(chalk.red('Exiting...'));
+          process.exit(1);
+        }
+        rl.close();
+        await startProcessing();
+      }
+    );
+  }
+};
