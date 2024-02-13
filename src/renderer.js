020d58a355fcbd8fbc4af1ced84d13a88dae9bbf
Shaomeng Zhang <randomor@gmail.com>
2024-02-12 20:45:36 -0800

    Properly render xml without whitespace

--- src/renderer.js
+++ src/renderer.js
@@ -44,12 +44,10 @@ export const renderSitemap = async (pageAttributesList, sitemapPath) => {
     })
     .join("\n");
 
-  const sitemapXml = `
-      <?xml version="1.0" encoding="UTF-8"?>
-      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        ${sitemap}
-      </urlset>
-      `;
+  const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${sitemap}
+</urlset>`;
 
   await fs.writeFile(sitemapPath, sitemapXml);
 
